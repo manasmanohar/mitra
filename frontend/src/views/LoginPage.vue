057cad349d07f315710d6ede4a783ca663357924
manasmanohar <21006907+manasmanohar@users.noreply.github.com>
2022-06-13 06:57:51 +0530

    added loginpage template

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <TopNav />
+        <section id="main">
+            <h1>Login to Mitra</h1>
+        </section>
+    </div>
+</template>
+
+<script>
+    import TopNav from '../components/TopNav.vue'
+    export default {}
+</script>
+
+<style></style>
+
+TopNav
